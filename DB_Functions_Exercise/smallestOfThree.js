5047d52c0b4f0b321d1e674c5c872b28d75fb2c5
yetoshimo <yetmenyilmaz@gmail.com>
2022-06-24 15:28:16 +0300

    functions exercises

---
+++ DB_Functions_Exercise/smallestOfThree.js
@@ -0,0 +1,21 @@
+function smallestOfThree(firstNumber, secondNumber, thirdNumber) {
+    console.log(Math.min(firstNumber, secondNumber, thirdNumber));
+}
+
+smallestOfThree(2,
+    5,
+    3
+)
+
+smallestOfThree(600,
+    342,
+    123)
+
+smallestOfThree(25,
+    21,
+    4)
+
+smallestOfThree(
+    2,
+    2,
+    2)
