a592fbeee116a055a4654e499c76940ce53740a7
yetoshimo <yetmenyilmaz@gmail.com>
2022-06-25 13:04:00 +0300

    added new exercise

--- DB_Functions_Exercise/perfectNumber.js
+++ DB_Functions_Exercise/perfectNumber.js
@@ -1,3 +1,19 @@
 function perfectNumber(givenNumber) {
-    
+    let divisionTotal = 0;
+    for (let i = 1; i < givenNumber; i++) {
+        if (givenNumber % i == 0) {
+            divisionTotal += i;
+        }
+    }
+    if (givenNumber == divisionTotal) {
+        console.log('We have a perfect number!');
+    } else {
+        console.log('It\'s not so perfect.')
+    }
 }
+
+perfectNumber(6)
+
+perfectNumber(28)
+
+perfectNumber(1236498)
